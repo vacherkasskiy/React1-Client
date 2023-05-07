b4a64258324a1fc01b3792e7e44cdb82e2cfa044
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-05-08 02:44:12 +0300

    added simple design to project

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import "./Body.css"
+
+function Body() {
+    return (
+        <div className="body">
+            Text here
+        </div>
+    );
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <p className="header__item">
+                Item1
+            </p>
+            <p className="header__item">
+                Item1
+            </p>
+            <p className="header__item">
+                Item1
+            </p>
+            <p className="header__item">
+                Item1
+            </p>
+        </header>
+    );
+}
+
+export default Header;
