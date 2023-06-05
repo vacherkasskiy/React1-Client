4498f7dcbb0a36a628c5864caffc3bb0c807c2d3
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-06-05 23:19:31 +0300

    api & container components merged

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,70 +1,24 @@
-import React from "react";
-import axios from "axios";
 import User from "./User";
+import React from "react";
 import "../../styles/Users/Users.css";
 
-class Users extends React.Component {
-    state = {
-        pages: []
-    }
-    sendAPIRequest() {
-        axios
-            .get(
-                "https://localhost:7072/users/get_users?skip=" +
-                ((this.props.currentPage - 1) * this.props.pageCapacity) +
-                "&amount=" +
-                this.props.pageCapacity
-            )
-            .then((response) => {
-                const users = response.data.users;
-                const usersAmount = response.data.usersAmount;
-
-                const pages = [];
-                for (let i = 1; i <= Math.ceil(usersAmount / this.props.pageCapacity); ++i) {
-                    pages.push(i);
-                }
-
-                this.props.setUsersAmount(usersAmount);
-                this.props.setUsers(users);
-
-                this.setState({ pages: pages });
-            });
-    }
-
-    componentDidMount() {
-        this.sendAPIRequest();
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.currentPage !== this.props.currentPage) {
-            this.sendAPIRequest();
-        }
-    }
-
-    pageOnClick(x) {
-        this.props.setCurrentPage(x);
-    }
-
-    render() {
-        const { pages } = this.state;
-
-        return (
-            <div className="users">
-                <div className="pages__buttons">
-                    {pages.map((x) => (
-                        <div
-                            key={x}
-                            onClick={() => this.pageOnClick(x)}
-                            className={x === this.props.currentPage ? "active" : ""}
-                        >{x}</div>
-                    ))}
-                </div>
-                {this.props.users.map((x) => (
-                    <User key={x.id} data={x} />
+function Users(props) {
+    return (
+        <div className="users">
+            <div className="pages__buttons">
+                {props.pages.map((x) => (
+                    <div
+                        key={x}
+                        onClick={() => props.pageOnClick(x)}
+                        className={x === props.currentPage ? "active" : ""}
+                    >{x}</div>
                 ))}
             </div>
-        );
-    }
+            {props.users.map((x) => (
+                <User key={x.id} data={x} />
+            ))}
+        </div>
+    );
 }
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,10 +1,65 @@
 import {connect} from "react-redux";
-import Users from "./Users";
 import {
     setCurrentPageActionCreator,
     setUsersActionCreator,
     setUsersAmountActionCreator
 } from "../../redux/reducers/user_reducer";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersAPIContainer extends React.Component {
+    state = {
+        pages: []
+    }
+
+    sendAPIRequest() {
+        axios
+            .get(
+                "https://localhost:7072/users/get_users?skip=" +
+                ((this.props.currentPage - 1) * this.props.pageCapacity) +
+                "&amount=" +
+                this.props.pageCapacity
+            )
+            .then((response) => {
+                const users = response.data.users;
+                const usersAmount = response.data.usersAmount;
+
+                const pages = [];
+                for (let i = 1; i <= Math.ceil(usersAmount / this.props.pageCapacity); ++i) {
+                    pages.push(i);
+                }
+
+                this.props.setUsersAmount(usersAmount);
+                this.props.setUsers(users);
+
+                this.setState({ pages: pages });
+            });
+    }
+
+    componentDidMount() {
+        this.sendAPIRequest();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.currentPage !== this.props.currentPage) {
+            this.sendAPIRequest();
+        }
+    }
+
+    render() {
+        const { pages } = this.state;
+
+        return (
+            <Users
+                pages={pages}
+                pageOnClick={this.props.setCurrentPage}
+                currentPage={this.props.currentPage}
+                users={this.props.users}
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -33,6 +88,6 @@ let mapDispatchToProps = (dispatch) => {
 };
 
 let UsersContainer =
-    connect(mapStateToProps, mapDispatchToProps)(Users);
+    connect(mapStateToProps, mapDispatchToProps)(UsersAPIContainer);
 
 export default UsersContainer;
