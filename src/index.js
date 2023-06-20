74c6d43e829e33f8e6b3d9f0984969cacf2ebd2a
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-06-21 01:11:02 +0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import reportWebVitals from './reportWebVitals';
-import "./styles/null.css";
-import "./styles/main.css";
-import ReactDOM from "react-dom/client";
-import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Content from "./components/Content";
-import store from "./redux/redux-store";
-import {Provider} from "react-redux";
-import axios from "axios";
-import LoginPageContainer from "./components/Auth/LoginPageContainer";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './styles/null.css';
+import './styles/main.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Content from './components/Content';
+import store from './redux/redux-store';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import LoginPageContainer from './components/Auth/LoginPageContainer';
 
 window.store = store;
-window.users = axios
-    .get("https://localhost:7072/users/get_users")
-    .then(response => response.data);
+window.users = axios.get('https://localhost:7072/users/get_users').then(response => response.data);
 
-let Render = () => {
+const Render = () => {
     const location = window.location.pathname;
 
-    root.render(
+    ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
                 <Provider store={store}>
@@ -34,17 +30,29 @@ let Render = () => {
                         </>
                     )}
                     <Routes>
-                        <Route path="/login" element={<LoginPageContainer/>} />
+                        <Route path="/login" element={<LoginPageContainer />} />
+                        <Route
+                            path="/"
+                            element={
+                                location === '/' ? (
+                                    <Navigate to="/profile/1" />
+                                ) : (
+                                    <>
+                                        <Header />
+                                        <Sidebar />
+                                        <Content />
+                                    </>
+                                )
+                            }
+                        />
                     </Routes>
                 </Provider>
             </BrowserRouter>
-        </React.StrictMode>
+        </React.StrictMode>,
+        document.getElementById('root')
     );
-}
+};
 
 Render();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
